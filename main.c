6e4d7f533e1f01cc1f01943ced45ef01ab15fbda
ClosedGL2 <closedgl64@protonmail.com>
2022-04-12 02:37:13 -0600

    Improvements

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
@@ -7,63 +8,81 @@
 #define DEBUG 0
 
 int main(int argc, char** argv);
-int render(double zoom, double move_x, double move_y);
+int render();
 int handle_events();
 void cleanup();
 
 #define DBG(fmt, ...) do { if (DEBUG) printf(fmt, __VA_ARGS__); } while (0)
 
+// texture
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 SDL_Texture *texture = NULL;
+SDL_Rect src = {0};
+
+// zoom and move and stuffs
+uint8_t fit_to_window = 1; // image appears no bigger than window
+double zoom = 1.0; // 100% initially
+double move_x = 0.0; // initial x pos (centered)
+double move_y = 0.0; // initial y pos (centered)
 
 int
 main(int argc, char** argv)
 {
+	// quit if file not specified
 	if(argc < 2)
 	{
 		fprintf(stderr, "Please provide a filename (or '-' to read from stdin).\n");
 		return 1;
 	}
 
+	// quit if file doesn't exist
 	if(strcmp(argv[1], "-") && access(argv[1], F_OK ) == -1)
 	{
 		fprintf(stderr, "Filename doesnt exist.\n");
 		return 1;
 	}
 
+	// init
 	SDL_Init(SDL_INIT_VIDEO);
 	IMG_Init(IMG_INIT_PNG);
 
 	atexit(cleanup);
 
-	char* windowtitle = malloc(sizeof(char) * (11 + strlen(strcmp(argv[1], "-") ? argv[1] : "stdin"))); // +9 bcz "OSXIV - "
+	// generate window title
+	char* windowtitle = malloc(sizeof(char) * (11 + strlen(strcmp(argv[1], "-") ? argv[1] : "stdin"))); // +11 for length of "QOIView - "
 	sprintf(windowtitle, "QOIView - %s", strcmp(argv[1], "-") ? argv[1] : "stdin");
 
+	// get screen dimensions - 50
 	SDL_DisplayMode dm;
 	SDL_GetCurrentDisplayMode(0, &dm);
+	dm.w -= 50;
+	dm.h -= 50;
 
+	// create window
 	window = SDL_CreateWindow(
 			windowtitle,
 			SDL_WINDOWPOS_UNDEFINED,
 			SDL_WINDOWPOS_UNDEFINED,
-			dm.w / 2, //w
-			dm.h / 2, //h
+			640, 480,
 			SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
 	);
-
 	free(windowtitle);
 
+	// create renderer
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE);
 
+	// open image file
+	// if argument is "-", will load from stdin
 	SDL_RWops* img;
-	if(!strcmp(argv[1], "-"))
+	if (!strcmp(argv[1], "-"))
 		img = SDL_RWFromFP(stdin, 0);
 	else
 		img = SDL_RWFromFile(argv[1], "rb");
+
+	// load image into texture
 	texture = IMG_LoadTexture_RW(renderer, img, 0);
-	if(!texture)
-	{
+	if (!texture) {
 		SDL_FreeRW(img);
 		fprintf(stderr, "Problem while loading the image: (%s).\n", argv[1]);
 		return 1;
@@ -71,8 +90,15 @@ main(int argc, char** argv)
 
 	SDL_FreeRW(img);
 
-	if(handle_events())
-	{
+	// resize window to image size
+	SDL_QueryTexture(texture, NULL, NULL, &src.w , &src.h);
+	if (src.w >= dm.w || src.h >= dm.h)
+		SDL_MaximizeWindow(window);
+	else
+		SDL_SetWindowSize(window, src.w, src.h);
+
+	// main event handler
+	if (handle_events()) {
 		fprintf(stderr, "Event handling exception.\n");
 		return 1;
 	}
@@ -81,14 +107,12 @@ main(int argc, char** argv)
 }
 
 int
-render(double zoom, double move_x, double move_y)
+render()
 {
-	SDL_Rect src = {0};
 	SDL_Rect dst = {0};
 	int screen_h, screen_w;
 
 	SDL_RenderClear(renderer);
-	SDL_QueryTexture(texture, NULL, NULL, &src.w , &src.h);
 
 	DBG("\nsrc w: %d h: %d x: %d y: %d\n", src.w, src.h, src.x, src.y);
 
@@ -98,15 +122,27 @@ render(double zoom, double move_x, double move_y)
 
 	DBG("scr w: %d h: %d\n", screen_w, screen_h);
 
+	if (fit_to_window) {
+		if (screen_w < src.w || screen_h < src.h) {
+			zoom = screen_h;
+			zoom /= src.h;
+			if (screen_w < src.w * zoom) {
+				zoom = screen_w;
+				zoom /= src.w;
+			}
+		} else
+			zoom = 1.0;
+	}
+
 	/* APPLY ZOOM */
-	dst.h *= zoom;
-	dst.w *= zoom;
+	dst.h = src.h * zoom;
+	dst.w = src.w * zoom;
 
 	DBG("zoom: %f\n", zoom);
 
 	/* REVERT IMAGE FILL */
 	double ratio = (double) src.w / src.h;
-	if(dst.h >= dst.w / ratio)
+	if (dst.h >= dst.w / ratio)
 		dst.h = dst.w / ratio;
 	else
 		dst.w = ratio * dst.h;
@@ -137,10 +173,6 @@ handle_events()
 	double zoom_multiplier = 0.1; // 10%
 	double move_multipler = 0.1; // 10%
 
-	double zoom = 1.0; // 100% initially
-	double move_x = 0.0; // initial x pos (centered)
-	double move_y = 0.0; // initial y pos (centered)
-
 	while(SDL_WaitEvent(&event))
 	{
 		//DBG("Event: 0x%x\n", event.type);
@@ -151,35 +183,60 @@ handle_events()
 				{
 					case '+': // zoom in
 					case 61: // +
+						fit_to_window = 0;
 						zoom += zoom_multiplier;
-						render(zoom, move_x, move_y);
+						render();
 						break;
 					case '-': // zoom out
+						fit_to_window = 0;
 						zoom -= zoom - zoom_multiplier < 0 ? 0 : zoom_multiplier;
-						render(zoom, move_x, move_y);
+						render();
 						break;
+					case '0': // reset zoom
+						fit_to_window = 0;
+						zoom = 1.0;
+						render();
+						break;
+					case '9': // fit to window
+						fit_to_window = 1;
+						render();
+						break;
+					case 'k':
 					case 1073741906: // up arrow - move up
 						move_y -= move_multipler / zoom;
-						render(zoom, move_x, move_y);
+						render();
 						break;
+					case 'j':
 					case 1073741905: // down arrow - move down
 						move_y += move_multipler / zoom;
-						render(zoom, move_x, move_y);
+						render();
 						break;
+					case 'h':
 					case 1073741904: // left arrow - move left
 						move_x -= move_multipler / zoom;
-						render(zoom, move_x, move_y);
+						render();
 						break;
+					case 'l':
 					case 1073741903: // right arrow - move right
 						move_x += move_multipler / zoom;
-						render(zoom, move_x, move_y);
+						render();
+						break;
+					case 'c': // reset move (center)
+						move_x = 0.0;
+						move_y = 0.0;
+						render();
 						break;
 					case 'q':
 						exit(0);
 				}
 				break;
 			 case SDL_WINDOWEVENT:
-				render(zoom, move_x, move_y);
+				switch (event.window.event) {
+					case SDL_WINDOWEVENT_SHOWN:
+					case SDL_WINDOWEVENT_EXPOSED:
+					case SDL_WINDOWEVENT_SIZE_CHANGED:
+						render(zoom, move_x, move_y);
+				}
 				break;
 			case SDL_QUIT:
 				exit(0);
